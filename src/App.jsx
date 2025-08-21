d1deea043cfe9a38c642307a209d5eeaf3ee3923
krlyeon <sooyeon8427@gmail.com>
2025-08-22 00:09:17 +0900

    [Fix] 변경사항 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Home from "./pages/Home.jsx";
 import ChosePage from "./pages/ChosePage.jsx"; // 채팅/선택 페이지
 import ChatPage from "./pages/ChatPage.jsx"; // 채팅/선택 페이지
 import ScorePage from "./pages/ScorePage.jsx"; // 원큐스코어 페이지
-
+import PrintshopPage from "./pages/PrintshopPage.jsx";
+import PrintshopRegister from "./pages/PrintshopRegister.jsx";
+import PrintshopRegister2 from "./pages/PrintshopRegister2.jsx";
+import PrintshopRegister3 from "./pages/PrintshopRegister3.jsx";
 
 export default function App() {
   return (
@@ -14,6 +17,10 @@ export default function App() {
         <Route path="/chose" element={<ChosePage />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/score" element={<ScorePage />} />
+        <Route path="/printshopPage" element={<PrintshopPage />} />
+        <Route path="/printshopRegister" element={<PrintshopRegister />} />
+        <Route path="/printshopRegister2" element={<PrintshopRegister2 />} />
+        <Route path="/printshopRegister3" element={<PrintshopRegister3 />} />
       </Routes>
     </BrowserRouter>
   );
