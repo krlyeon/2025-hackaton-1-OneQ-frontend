32a1f460711ab32bb50e86ba80e504c0f589d9c1
krlyeon <sooyeon8427@gmail.com>
2025-08-22 15:43:14 +0900

    [Fix] Config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 배포 MIME 오류 예방: 번들 경로 절대경로
   server: {
     proxy: {
       '/api': {
-        target: 'https://yumiykim.shop/api/', // 백엔드 서버 주소 (예: Django, Node, Spring 등)
+        target: 'https://yumiykim.shop', // ← 여기서 /api 빼기!
         changeOrigin: true,
         secure: false,
+        // rewrite 불필요: /api 접두사는 그대로 유지됨
       }
     }
   }
